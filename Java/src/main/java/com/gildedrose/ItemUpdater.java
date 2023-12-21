972ac3a902690406d3115db4fd414e9e0557cfc6
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-12-21 01:13:48 -0500

    Implement new update quality method

--- Java/src/main/java/com/gildedrose/ItemUpdater.java
+++ Java/src/main/java/com/gildedrose/ItemUpdater.java
@@ -1,28 +1,32 @@
 package com.gildedrose;
 
 public interface ItemUpdater {
+  default int updateQuality(int current, int delta) {
+    return Math.max(0, current + delta);
+  }
+
   default void updateItem(Item item) {
     if (!item.getName().equals(GildedRose.AGED_BRIE)
         && !item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
       if (item.getQuality() > 0) {
         if (!item.getName().equals(GildedRose.SULFURAS)) {
-          item.setQuality(item.getQuality() - 1);
+          item.setQuality(this.updateQuality(item.getQuality(), -1));
         }
       }
     } else {
       if (item.getQuality() < 50) {
-        item.setQuality(item.getQuality() + 1);
+        item.setQuality(this.updateQuality(item.getQuality(), 1));
 
         if (item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
           if (item.getSellIn() < 11) {
             if (item.getQuality() < 50) {
-              item.setQuality(item.getQuality() + 1);
+              item.setQuality(this.updateQuality(item.getQuality(), 1));
             }
           }
 
           if (item.getSellIn() < 6) {
             if (item.getQuality() < 50) {
-              item.setQuality(item.getQuality() + 1);
+              item.setQuality(this.updateQuality(item.getQuality(), 1));
             }
           }
         }
@@ -38,15 +42,15 @@ public interface ItemUpdater {
         if (!item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
           if (item.getQuality() > 0) {
             if (!item.getName().equals(GildedRose.SULFURAS)) {
-              item.setQuality(item.getQuality() - 1);
+              item.setQuality(this.updateQuality(item.getQuality(), -1));
             }
           }
         } else {
-          item.setQuality(item.getQuality() - item.getQuality());
+          item.setQuality(0);
         }
       } else {
         if (item.getQuality() < 50) {
-          item.setQuality(item.getQuality() + 1);
+          item.setQuality(this.updateQuality(item.getQuality(), 1));
         }
       }
     }
