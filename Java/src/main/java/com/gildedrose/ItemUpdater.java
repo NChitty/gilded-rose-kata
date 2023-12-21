f86a3dfc87d25bb1f72ae50dfb515bf24d4388bf
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-12-21 01:53:25 -0500

    Flatten out bottom where sellIn less than zero

--- Java/src/main/java/com/gildedrose/ItemUpdater.java
+++ Java/src/main/java/com/gildedrose/ItemUpdater.java
@@ -31,22 +31,17 @@ public interface ItemUpdater {
       }
     }
 
-    if (item.getSellIn() < 0) {
-      if (!item.getName().equals(GildedRose.AGED_BRIE)) {
-        if (!item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
-          if (item.getQuality() > 0) {
-            if (!item.getName().equals(GildedRose.SULFURAS)) {
-              item.setQuality(this.updateQuality(item.getQuality(), -1));
-            }
-          }
-        } else {
-          item.setQuality(0);
-        }
-      } else {
-        if (item.getQuality() < 50) {
-          item.setQuality(this.updateQuality(item.getQuality(), 1));
-        }
-      }
+    if (item.getSellIn() >= 0) return;
+    if (item.getName().equals(GildedRose.AGED_BRIE)) {
+      if (item.getQuality() >= 50) return;
+      item.setQuality(this.updateQuality(item.getQuality(), 1));
+      return;
     }
+
+    if (item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
+      item.setQuality(0);
+      return;
+    }
+    item.setQuality(this.updateQuality(item.getQuality(), -1));
   }
 }
