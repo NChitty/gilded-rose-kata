f5b6f7fe78d797138788aeaa421173e97ee6f028
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-12-21 01:06:40 -0500

    Yank nasty logic to ItemUpdater default impl

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,17 @@
 package com.gildedrose;
 
 class GildedRose {
-  Item[] items;
+  private final Item[] items;
+  private final ItemUpdater itemUpdater;
 
   public GildedRose(Item[] items) {
     this.items = items;
+    this.itemUpdater = new ItemUpdater() {
+      @Override
+      public void updateItem(Item item) {
+        ItemUpdater.super.updateItem(item);
+      }
+    };
   }
 
   static final String AGED_BRIE = "Aged Brie";
@@ -13,54 +20,11 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (!items[ i ].getName().equals(AGED_BRIE)
-          && !items[ i ].getName().equals(BACKSTAGE_PASS)) {
-        if (items[ i ].getQuality() > 0) {
-          if (!items[ i ].getName().equals(SULFURAS)) {
-            items[ i ].setQuality(items[ i ].getQuality() - 1);
-          }
-        }
-      } else {
-        if (items[ i ].getQuality() < 50) {
-          items[ i ].setQuality(items[ i ].getQuality() + 1);
-
-          if (items[ i ].getName().equals(BACKSTAGE_PASS)) {
-            if (items[ i ].getSellIn() < 11) {
-              if (items[ i ].getQuality() < 50) {
-                items[ i ].setQuality(items[ i ].getQuality() + 1);
-              }
-            }
-
-            if (items[ i ].getSellIn() < 6) {
-              if (items[ i ].getQuality() < 50) {
-                items[ i ].setQuality(items[ i ].getQuality() + 1);
-              }
-            }
-          }
-        }
-      }
-
-      if (!items[ i ].getName().equals(SULFURAS)) {
-        items[ i ].setSellIn(items[ i ].getSellIn() - 1);
-      }
-
-      if (items[ i ].getSellIn() < 0) {
-        if (!items[ i ].getName().equals(AGED_BRIE)) {
-          if (!items[ i ].getName().equals(BACKSTAGE_PASS)) {
-            if (items[ i ].getQuality() > 0) {
-              if (!items[ i ].getName().equals(SULFURAS)) {
-                items[ i ].setQuality(items[ i ].getQuality() - 1);
-              }
-            }
-          } else {
-            items[ i ].setQuality(items[ i ].getQuality() - items[ i ].getQuality());
-          }
-        } else {
-          if (items[ i ].getQuality() < 50) {
-            items[ i ].setQuality(items[ i ].getQuality() + 1);
-          }
-        }
-      }
+      this.itemUpdater.updateItem(items[ i ]);
     }
+  }
+
+  public Item[] getItems() {
+    return this.items;
   }
 }
---
+++ Java/src/main/java/com/gildedrose/ItemUpdater.java
@@ -0,0 +1,54 @@
+package com.gildedrose;
+
+public interface ItemUpdater {
+  default void updateItem(Item item) {
+    if (!item.getName().equals(GildedRose.AGED_BRIE)
+        && !item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
+      if (item.getQuality() > 0) {
+        if (!item.getName().equals(GildedRose.SULFURAS)) {
+          item.setQuality(item.getQuality() - 1);
+        }
+      }
+    } else {
+      if (item.getQuality() < 50) {
+        item.setQuality(item.getQuality() + 1);
+
+        if (item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
+          if (item.getSellIn() < 11) {
+            if (item.getQuality() < 50) {
+              item.setQuality(item.getQuality() + 1);
+            }
+          }
+
+          if (item.getSellIn() < 6) {
+            if (item.getQuality() < 50) {
+              item.setQuality(item.getQuality() + 1);
+            }
+          }
+        }
+      }
+    }
+
+    if (!item.getName().equals(GildedRose.SULFURAS)) {
+      item.setSellIn(item.getSellIn() - 1);
+    }
+
+    if (item.getSellIn() < 0) {
+      if (!item.getName().equals(GildedRose.AGED_BRIE)) {
+        if (!item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
+          if (item.getQuality() > 0) {
+            if (!item.getName().equals(GildedRose.SULFURAS)) {
+              item.setQuality(item.getQuality() - 1);
+            }
+          }
+        } else {
+          item.setQuality(item.getQuality() - item.getQuality());
+        }
+      } else {
+        if (item.getQuality() < 50) {
+          item.setQuality(item.getQuality() + 1);
+        }
+      }
+    }
+  }
+}
