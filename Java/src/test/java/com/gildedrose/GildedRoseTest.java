c5d9c7cb2558cc2f20bdfc9fc704d91c78101659
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-12-21 00:47:20 -0500

    Finished tests and refactor to use getters and setters

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,214 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+  @Test
+  void foo() {
+    Item[] items = new Item[] { new Item("foo", 0, 0) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("foo", app.items[ 0 ].getName());
+  }
+
+  @Nested
+  class WhenBasicItem {
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void givenNormal_thenBothSellInAndQualityDecreases() {
+      Item item = new Item("foo", 1, 1);
+      GildedRose app = new GildedRose(new Item[] { item });
+      app.updateQuality();
+      assertEquals(0, item.getQuality());
+      assertEquals(0, item.getSellIn());
     }
 
+    @Test
+    void givenSellInZero_thenQualityDecreasesDouble() {
+      Item item = new Item("foo", 0, 4);
+      GildedRose app = new GildedRose(new Item[] { item });
+      app.updateQuality();
+      assertEquals(2, item.getQuality());
+      assertEquals(-1, item.getSellIn());
+    }
+
+    @Test
+    void givenSellInLessThanZero_thenQualityDecreasesDouble() {
+      Item item = new Item("foo", -1, 2);
+      GildedRose app = new GildedRose(new Item[] { item });
+      app.updateQuality();
+      assertEquals(0, item.getQuality());
+      assertEquals(-2, item.getSellIn());
+    }
+
+    @Test
+    void givenQualityIsZero_thenQualityRemainsZero() {
+      Item item = new Item("foo", 1, 0);
+      GildedRose app = new GildedRose(new Item[] { item });
+      app.updateQuality();
+      assertEquals(0, item.getQuality());
+      assertEquals(0, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { -1, -2, -3, -4, -5, -10, -100, Integer.MIN_VALUE })
+    void givenQualityLessThanZero_thenQualityUnmoved(int quality) {
+      Item item = new Item("foo", 1, quality);
+      GildedRose app = new GildedRose(new Item[] { item });
+      app.updateQuality();
+      assertEquals(quality, item.getQuality());
+      assertEquals(0, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 52, 53, 54, 55, 56, 60, 100, 1000, Integer.MAX_VALUE })
+    void givenQualityGreaterThanFifty_thenQualityDecreasesNormally(int quality) {
+      Item item = new Item("foo", 1, quality);
+      GildedRose app = new GildedRose(new Item[] { item });
+      app.updateQuality();
+      assertEquals(quality - 1, item.getQuality());
+      assertEquals(0, item.getSellIn());
+    }
+  }
+
+  @Nested
+  class WhenAgedItem {
+    @Test
+    void givenAgedBrie_thenQualityIncreases() {
+      Item item = new Item("Aged Brie", 1, 0);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(1, item.getQuality());
+      assertEquals(0, item.getSellIn());
+    }
+
+    @Test
+    void givenPassSellIn_thenQualityIncreasesDouble() {
+      Item item = new Item("Aged Brie", 0, 0);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(2, item.getQuality());
+      assertEquals(-1, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 50, 51, 52, 53, 54, 60, 100, 1000, Integer.MAX_VALUE })
+    void givenQualityIsFiftyOrGreater_thenQualityUnchanged(int quality) {
+      Item item = new Item("Aged Brie", 1, quality);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(quality, item.getQuality());
+      assertEquals(0, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,50", "0,51", "-1,50", "-1,51"})
+    void givenSellInZeroOrLess_thenQualityUnchanged(int sellIn, int quality) {
+      Item item = new Item("Aged Brie", sellIn, quality);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(quality, item.getQuality());
+      assertEquals(sellIn - 1, item.getSellIn());
+    }
+  }
+
+  @Nested
+  class WhenLegendaryItem {
+    @Test
+    void givenSulfuras_thenRemainUnchanged() {
+      Item item = new Item("Sulfuras, Hand of Ragnaros", 1, 1);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(1, item.getQuality());
+      assertEquals(1, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, -1, 10, -10, Integer.MAX_VALUE, Integer.MIN_VALUE})
+    void givenVariousSellIn_thenRemainUnchanged(int sellIn) {
+      Item item = new Item("Sulfuras, Hand of Ragnaros", sellIn, 1);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(1, item.getQuality());
+      assertEquals(sellIn, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, -1, 10, -10, Integer.MAX_VALUE, Integer.MIN_VALUE})
+    void givenVariousQualities_thenRemainUnchanged(int quality) {
+      Item item = new Item("Sulfuras, Hand of Ragnaros", 1, quality);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(quality, item.getQuality());
+      assertEquals(1, item.getSellIn());
+    }
+  }
+
+  @Nested
+  class WhenSpecialtyItem {
+    @Test
+    void givenBackStagePass_thenQualityIncreases() {
+      Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 1);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(3, item.getQuality());
+      assertEquals(9, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {11, 12, 13, 14, 15, 100, 1000, Integer.MAX_VALUE})
+    void givenSellInGreaterThanTen_thenQualityIncreasesByOne(int sellIn) {
+      Item item = new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, 0);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(1, item.getQuality());
+      assertEquals(sellIn - 1, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10, 9, 8, 7, 6})
+    void givenSellInLessThanEqualTenAndGreaterThan5_thenQualityIncreasesByTwo(int sellIn) {
+      Item item = new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, 0);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(2, item.getQuality());
+      assertEquals(sellIn - 1, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 4, 3, 2, 1})
+    void givenSellInLessThanEqualFive_thenQualityIncreasesByThree(int sellIn) {
+      Item item = new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, 0);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(3, item.getQuality());
+      assertEquals(sellIn - 1, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -2, -10, -100, Integer.MIN_VALUE})
+    void givenSellInLessThanEqualZero_thenQualityClampedToZero(int sellIn) {
+      Item item = new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, -1);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(0, item.getQuality());
+      assertEquals(sellIn - 1, item.getSellIn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 50, 51, 52, 53, 54, 60, 100, 1000, Integer.MAX_VALUE })
+    void givenQualityIsFiftyOrGreater_thenQualityUnchanged(int quality) {
+      Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, quality);
+      GildedRose app = new GildedRose(new Item[] { item});
+      app.updateQuality();
+      assertEquals(quality, item.getQuality());
+      assertEquals(0, item.getSellIn());
+    }
+  }
 }
