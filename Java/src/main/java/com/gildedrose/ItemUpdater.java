304bb01fa1d39a7a65d7f53ac357ff5a791fd11d
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-12-21 02:06:42 -0500

    Unite item logic by name

--- Java/src/main/java/com/gildedrose/ItemUpdater.java
+++ Java/src/main/java/com/gildedrose/ItemUpdater.java
@@ -9,38 +9,37 @@ public interface ItemUpdater {
 
   default void updateItem(Item item) {
     if (item.getName().equals(GildedRose.SULFURAS)) return;
+
     item.setSellIn(item.getSellIn() - 1);
 
-    if (!item.getName().equals(GildedRose.AGED_BRIE)
-        && !item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
-      item.setQuality(this.updateQuality(item.getQuality(), -1));
-    } else {
-      if (item.getQuality() < 50) {
-        item.setQuality(this.updateQuality(item.getQuality(), 1));
-
-        if (item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
-          if (item.getSellIn() >= 10) return;
-          if (item.getQuality() >= 50) return;
-          item.setQuality(this.updateQuality(item.getQuality(), 1));
-
-          if (item.getSellIn() >= 5) return;
-          if (item.getQuality() >= 50) return;
-          item.setQuality(this.updateQuality(item.getQuality(), 1));
-        }
-      }
-    }
-
-    if (item.getSellIn() >= 0) return;
-    if (item.getQuality() >= 50) return;
     if (item.getName().equals(GildedRose.AGED_BRIE)) {
+      if (item.getQuality() >= 50) return;
+      item.setQuality(this.updateQuality(item.getQuality(), 1));
+
+      if (item.getSellIn() >= 0) return;
       item.setQuality(this.updateQuality(item.getQuality(), 1));
       return;
     }
 
     if (item.getName().equals(GildedRose.BACKSTAGE_PASS)) {
+      if (item.getQuality() >= 50) return;
+      item.setQuality(this.updateQuality(item.getQuality(), 1));
+      if (item.getSellIn() >= 10) return;
+      if (item.getQuality() >= 50) return;
+      item.setQuality(this.updateQuality(item.getQuality(), 1));
+
+      if (item.getSellIn() >= 5) return;
+      if (item.getQuality() >= 50) return;
+      item.setQuality(this.updateQuality(item.getQuality(), 1));
+
+      if (item.getSellIn() >= 0) return;
       item.setQuality(0);
       return;
     }
+
+    item.setQuality(this.updateQuality(item.getQuality(), -1));
+
+    if (item.getSellIn() >= 0) return;
     item.setQuality(this.updateQuality(item.getQuality(), -1));
   }
 }
